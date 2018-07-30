9383750a01d8a944fbbbf58957d451e043a006fc
venture21 <park0122@gmail.com>
2018-07-30 16:20:23 +0900

    update calculator

--- app/src/main/java/com/venture21/calculator/WidgetActivity.java
+++ app/src/main/java/com/venture21/calculator/WidgetActivity.java
@@ -2,10 +2,16 @@ package com.venture21.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class WidgetActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
@@ -13,8 +19,8 @@ public class WidgetActivity extends AppCompatActivity implements CompoundButton.
     CheckBox cbBanana;
     CheckBox cbCherry;
     RadioGroup rg;
-
-
+    Spinner sp;
+    ArrayList<String> years = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,30 @@ public class WidgetActivity extends AppCompatActivity implements CompoundButton.
         cbApple.setOnCheckedChangeListener(this);
         cbBanana.setOnCheckedChangeListener(this);
         cbCherry.setOnCheckedChangeListener(this);
+
+        sp = (Spinner) findViewById(R.id.spinner);
+
+        //데이터는 올해부터 ~ 100년전까지
+        for(int i=2018;i>2018-100;i--){
+            years.add(i + "");
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,android.R.layout.simple_spinner_dropdown_item, years
+                //컨텍스트,                스피너에서 사용할 레이아웃          배열데이터
+        );
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(WidgetActivity.this, "선택된아이템="+years.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         rg = (RadioGroup) findViewById(R.id.radiogroup);
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
